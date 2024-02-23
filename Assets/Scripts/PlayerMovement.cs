e8708ee5edc3eed66ba8619c5cf30cd91797c6ac
Henz00 <henzoo.hansen@gmail.com>
2024-02-23 01:21:58 +0100

    Added player movement, created basic test scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float movementSpeed = 10;
+
+    float xAxis;
+    float yAxis;
+
+    // Update is called once per frame
+    void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(xAxis * movementSpeed, yAxis * movementSpeed, 0);
+        movement *= Time.deltaTime*10;
+        transform.Translate(movement);
+    }
+}
